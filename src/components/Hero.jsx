81021eedfedb79a59e0943edbd40ff5f60584e7e
Prabin Sharma <Prabin@Phadendras-MacBook-Pro.local>
2023-08-30 01:05:14 -0400

    undo to original

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { logos } from "../assets";
+import { logo } from "../assets";
 const Hero = () => {
   return (
         <header className='w-full flex justify-center items-center flex-col'>
           {/* </header><nav className='flex justify-between items-center w-full mb-10 pt-3'> */}
           <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-          <img src={logos} alt='sumz_logo' className='w-28 object-contain' />
+          <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
 
 <button
   type='button'
