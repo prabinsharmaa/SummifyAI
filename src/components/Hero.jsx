dfcbfe43752342456dc1640dd10885a4563bf862
Prabin Sharma <Prabin@Phadendras-MacBook-Pro.local>
2023-08-29 21:46:50 -0400

    Logo changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { logo } from "../assets";
+import { logos } from "../assets";
 const Hero = () => {
   return (
         <header className='w-full flex justify-center items-center flex-col'>
